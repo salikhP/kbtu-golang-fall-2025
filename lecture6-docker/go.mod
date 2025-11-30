8556b4104379bd633188100720ce6fe017689faf
SalikhP <pernebek.sm@gmail.com>
2025-11-30 20:58:40 +0500

    Docker with air example added

---
+++ lecture6-docker/go.mod
@@ -0,0 +1,3 @@
+module lecture-6
+
+go 1.23.2
---
+++ lecture6-docker/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Docker!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+// for testing: curl localhost:8080/
